e482218073b5ed737053d329d8889e8ecc4cae00
Róslín <roslin19@ru.is>
2020-05-03 14:43:04 +0000

    started working in the models stuff

--- CaptainConsole/Accessories/models.py
+++ CaptainConsole/Accessories/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Accessories(models.Model):
+    name = models.charfield(max_length=2555)
+    description = models.charfield(max_length=999, blank=True)
--- CaptainConsole/AllProducts/apps.py
+++ CaptainConsole/AllProducts/apps.py
@@ -4,17 +4,19 @@ from django.apps import AppConfig
 class AllproductsConfig(AppConfig):
     name = 'AllProducts'
 
+
 class InsertProduct():
     def __init__(self):
         pass
 
-    def DeleteProduct(self):
+    def deleteProduct(self):
         pass
 
     def ChangeProduct(self):
         pass
 
-#ekki alveg viss í hvaða packages þetta þarf að fara, svo skal ég setja það hér
+
+# ekki alveg viss í hvaða packages þetta þarf að fara, svo skal ég setja það hér
 
 class Customer():
     def __init__(self):
@@ -25,9 +27,10 @@ class Customer():
 
     def signUp(self):
         pass
-#vantar að bætta við search history og payment info í cutomer
+
+
+# vantar að bætta við search history og payment info í cutomer
 
 class CaptainConsole():
     def __init__(self):
         pass
-
