3f48ad9e87201606ae3eec25ac32f469b20c2ef9
Róslín <roslin19@ru.is>
2020-05-04 14:08:37 +0000

    deleted the database settings file

--- CaptainConsole/CaptainConsole/settings.py
+++ CaptainConsole/CaptainConsole/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'CaptainConsole.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tygsvetx',
+        'USER': 'tygsvetx',
+        'PASSWORD': 'iU1GNNSfF7a8FlS9fwpJNeTLebwfMkZ3',
+        'HOST': 'balarama.db.elephantsql.com',
+        'PORT': '5432'
     }
 }
 
