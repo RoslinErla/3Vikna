c149c5f72ae084ba265dfc545c6225ee53023f83
Róslín <roslin19@ru.is>
2020-05-04 23:48:32 +0000

    did the all product page and added some style. It's not perfect but it is a start

--- CaptainConsole/AllProducts/models.py
+++ CaptainConsole/AllProducts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=999, blank=True)
@@ -7,6 +8,13 @@ class Product(models.Model):
     manufacturer = models.CharField(max_length=255)
     type = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
+
 class ProductImage(models.Model):
     image = models.CharField(max_length=999)
     Product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.image
