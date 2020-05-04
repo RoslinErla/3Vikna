636703534dc995d6fcc5b6a45c4379d59173e037
Jevgenija Zlotnikova <zlotnikova.jevgenija@gmail.com>
2020-05-04 11:31:56 +0000

    cc

--- CaptainConsole/Consoles/models.py
+++ CaptainConsole/Consoles/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+# these models sould be setup: frá database setup leture
+# Candy - name, description, cathegory, price, on_sale, manufacturer
+# CandyImage - image, candy
+# CandyCategory - name
+# Manufacturer - name, year_of_start, logo
+
+class CaptainConsole(models.model):
