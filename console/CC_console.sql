a41fdc64547d41db39413a80a6996b97d6429763
Jevgenija Zlotnikova <zlotnikova.jevgenija@gmail.com>
2020-05-04 21:40:08 +0000

    folder rename

---
+++ console/CC_console.sql
@@ -0,0 +1,19 @@
+INSERT INTO Product (name, description, price, manufacturer, type) 
+VALUES ('Nintendo 64', 'Nintendo 64 is the third generation of video game console from Nintendo. It was introduced in 1996. Nintendo is a company whose very name is synonymous with video gaming.', 30.000, 'Nintendo 64', 'Games');
+INSERT INTO Product (name, description, price, manufacturer, type) 
+VALUES ('Atari 2600', 'The Atari 2600, originally branded as the Atari Video Computer System or Atari VCS for short until November 1982, is a home video game console from Atari, Inc.', 25.000, 'PlayStation 2', 'Games');
+INSERT INTO Product (name, description, price, manufacturer, type) 
+VALUES ('Playstation', 'The PlayStation, one of a new generation of 32-bit consoles, signaled Sonys rise to power in the video game world. Also known as the PS One, the PlayStation used compact discs (CDs), heralding the video game industry´s move away from cartridges.', 27.000, 'Atari 2600', 'Games');
+INSERT INTO Product (name, description, price, manufacturer, type) 
+VALUES ('Sega Genesis', 'The Sega Genesis, known as the Mega Drive outside North America, is a 16-bit home video game console developed and sold by Sega. The Genesis is Segas third console and the successor to the Master System. Sega released it as the Mega Drive in Japan in 1988, and later as the Genesis in North America in 1989.', 28.000, 'Sega Genesis', 'Games');
+
+INSERT INTO ProductImage (products, image)
+VALUES ('Nintendo 64', 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Nintendo-64-wController-L.jpg/600px-Nintendo-64-wController-L.jpg');
+INSERT INTO ProductImage (products, image)
+VALUES ('Playstation', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/PSX-Console-wController.jpg/500px-PSX-Console-wController.jpg');
+INSERT INTO ProductImage (products, image)
+VALUES ('Atari 2600', 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Atari-2600-Wood-4Sw-Set.jpg/600px-Atari-2600-Wood-4Sw-Set.jpg');
+INSERT INTO ProductImage (products, image)
+VALUES ('Sega Genesis', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Sega-Mega-Drive-JP-Mk1-Console-Set.jpg/500px-Sega-Mega-Drive-JP-Mk1-Console-Set.jpg');
+
+
