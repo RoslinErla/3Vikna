8fe52648d78e05918c238b721167946d7a0a15c5
Jevgenija Zlotnikova <zlotnikova.jevgenija@gmail.com>
2020-05-07 10:25:50 +0000

    urls

--- CaptainConsole/Admin/urls.py
+++ CaptainConsole/Admin/urls.py
@@ -1,8 +1,9 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.register, name="Admin-index"),
-    path('login', Loginview.as_view(template_name='admin/login.html',name='login'),
-    path('logout', LogoutView.as_view(next_page='login'), name = 'logout')]
+    path('login', LoginView.as_view(template_name='admin/login.html'), name='login'),
+    path('logout', LogoutView.as_view(next_page='login'), name = 'logout')
+]
