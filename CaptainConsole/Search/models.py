a29909108510b370c43742b9d3dc2e32bc56fe29
Róslín <roslin19@ru.is>
2020-05-07 12:27:00 +0000

    added the search app

---
+++ CaptainConsole/Search/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=999, blank=True)
+    price = models.FloatField()
+    manufacturer = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductImage(models.Model):
+    image = models.CharField(max_length=999)
+    Product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.image
