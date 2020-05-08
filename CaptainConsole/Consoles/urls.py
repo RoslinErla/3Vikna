f5babfd5110e9ab2ad23788e4b93d47ef6de2b42
Róslín <roslin19@ru.is>
2020-05-08 10:10:44 +0000

    worked on connecting the navbar#

--- CaptainConsole/Consoles/urls.py
+++ CaptainConsole/Consoles/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="consoles-index"),
-    path('', views.index2, name="consoles-index2")
+    path('', views.index2, name="consoles-index2"),
+    path('nintendo', views.index3, name='consoles-index3'),
+    path('atari', views.index4, name='consoles-index4',),
+    path('sega', views.index5, name='consoles-index5'),
+    path('playstation', views.index6, name='consoles-index6')
 ]
--- CaptainConsole/Consoles/views.py
+++ CaptainConsole/Consoles/views.py
@@ -13,3 +13,25 @@ def index(request):
 def index2(request):
     context = {'products': Product.objects.filter(type='Consoles').order_by('price')}
     return render(request, 'consoles/index.html', context)
+
+
+def index3(request):
+    context = {'products': Product.objects.filter(type='Consoles').filter(manufacturer__icontains='Nintendo')}
+    return render(request, 'consoles/index.html', context)
+
+
+def index4(request):
+    context = {'products': Product.objects.filter(type='Consoles').filter(manufacturer__icontains='Atari')}
+    return render(request, 'consoles/index.html', context)
+
+
+def index5(request):
+    context = {'products': Product.objects.filter(type='Consoles').filter(manufacturer__icontains='sega')}
+    return render(request, 'consoles/index.html', context)
+
+
+def index6(request):
+    context = {'products': Product.objects.filter(type='Consoles').filter(manufacturer__icontains='playstation')}
+    return render(request, 'consoles/index.html', context)
+
+
