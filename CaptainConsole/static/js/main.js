4405ca03403d0c702ff34f22612c2b5b0714dd4b
Róslín <roslin19@ru.is>
2020-05-11 22:40:31 +0000

    some fikt

--- CaptainConsole/static/js/main.js
+++ CaptainConsole/static/js/main.js
@@ -32,6 +32,40 @@ $(document).ready(function(){
     })
 })
 
+$(document).ready(function(){
+    $('#filter-btn').on('click', function(e) {
+        e.preventDefault();
+        let searchText = $('#search-box').val();
+        console.log(searchText)
+        $.ajax({
+            url: '?search_filter=' + searchText,
+            type: 'GET',
+            success: function (resp) {
+                let newHtml = resp.data.map(d => {
+                    return `<div class="one product"> 
+                                <a href="/${d.id}">
+                                    <img class="product-image" src="${d.firstImage}"/>
+                                    <h4>${d.name}</h4>
+                                    <p>${d.price} isk</p>
+                                </a>
+                            </div>`
+                });
+                $('h4').empty()
+                $('h1').empty()
+                $('h2').empty()
+                $('.product-details').remove()
+                $('.show').empty()
+                $('.products').html(newHtml.join(''));
+                $('#search-box').val();
+            },
+            error: function (xhr, status, error) {
+                console.log(error);
+
+            }
+        })
+    })
+})
+
 
 jQuery(function($) {
  let path = window.location.href; // because the 'href' property of the DOM element is the absolute path
