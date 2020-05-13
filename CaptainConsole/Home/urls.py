896104cde6905484db45709e51e35c4f73b72b40
HBreki <hbvalsson@gmail.com>
2020-05-13 16:54:47 +0000

    button code for description page

--- CaptainConsole/Home/urls.py
+++ CaptainConsole/Home/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="home-index"),
-    path('<int:id>', views.get_product_by_id, name="home_Product_details")
-
+    path('<int:id>', views.get_product_by_id, name="home_Product_details"),
+    path('add_to_cart/<int:id>', views.add_to_cart, name="add-to-cart")
 ]
