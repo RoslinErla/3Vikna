1d95ca543a51d5d20d2dc734190c751c84b7e948
Róslín <roslin19@ru.is>
2020-05-14 16:21:44 +0000

    final js update

--- CaptainConsole/static/js/main.js
+++ CaptainConsole/static/js/main.js
@@ -25,6 +25,7 @@ $(document).ready(function(){
                 $('.back-button').remove()
                 $('#purchase').remove()
                 $('.profile').remove()
+                $('.btn-block').remove()
                 $('.products').empty()
                 $('.products').html(newHtml.join(''));
                 $('#search-box').val();
@@ -45,77 +46,6 @@ jQuery(function($) {
   })
  });
 
-let itemRemover = document.getElementsByClassName('remove-button')
-for (let i = 0; i <itemRemover.length; i++) {
- let button = itemRemover[i]
- button.addEventListener('click', cartItemRemover)
-}
 
-    let inputsToItemQuantity = document.getElementsByClassName('cart-quantity')
-    for (let i = 0; i < inputsToItemQuantity.length; i++) {
-        let input = inputsToItemQuantity[i]
-        input.addEventListener('change', changingQuantity)
-    }
-
-let itemToCartButton = document.getElementsByClassName('add-to-cart-btn')
-for (let i = 0; i <itemToCartButton.length; i++) {
-    let button = itemToCartButton[i]
-    button.addEventListener('click', cartAddClick)
-}
-
-function cartAddClick(event){
-    let button = event.target
-    let singleProductList = button.id.split(",")
-    console.log(singleProductList)
-    let productName = singleProductList[1]
-    let productPrice = singleProductList[2]
-    let productImgSrc = document.getElementById(singleProductList[0]).src
-    console.log(productName, productPrice, productImgSrc)
-    moveItemToCart(productName, productPrice, productImgSrc)
-}
-
-//function moveItemToCart(productName, productPrice, productImgSrc){
-//    localStorage.setItem('product-name', productName)
-//    console.log(localStorage)
-//}
-
-function moveItemToCart(productName, productPrice, productImgSrc){
-    let newItem = document.createElement('div')
-    newItem.classList.add('all-items')
-    //let itemsInCart = document.getElementsByClassName('all-items')[0]
-    let cartRowContents = `
-        <div class="cart-item-info">
-        <img class="cart-item-image" src="${productImgSrc}" width="100" height="100">
-        &emsp;
-        <p class="cart-item-title">${productName}</p>
-        &emsp;
-        <span class="price">${productPrice}</span>
-        &emsp;
-        <input class="cart-quantity" type="number" value="1">
-        &emsp;
-        <button class="remove-button" type="button">X</button>
-        </div>`
-    newItem.innerHTML = cartRowContents
-    console.log(newItem)
-
-    document.getElementsByClassName('all-items')[0].append(newItem)
-}
-
-function changingQuantity(event) {
-    let input = event.target
-    if(isNaN(input.value) || input.value <=0){
-        input.value = 1
-    }
-    if(isNaN(input.value) || 5 < input.value){
-        input.value = 5
-    }
-    updateTotal()
-}
-
-function cartItemRemover(event){
-  let onClick = event.target
-  onClick.parentElement.remove()
-  updateTotal()
-}
 
 
