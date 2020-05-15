eea58c949cebdcc717b44a2d21ebabdd22fbc00c
Róslín <roslin19@ru.is>
2020-05-15 14:03:22 +0000

    some final changes and fixes

--- CaptainConsole/Accessories/urls.py
+++ CaptainConsole/Accessories/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name="accessories-index"),
     path('price/', views.index2, name="accessories-index2"),
-    path('price/<int:id>',views.get_product_by_id, name="accessories_Product_details"),
+    path('price/<int:id>', views.get_product_by_id, name="accessories_Product_details"),
     path('nintendo', views.index3, name="accessories-index3"),
     path('nintendo/<int:id>', views.get_product_by_id, name="accessories_Product_details"),
     path('atari', views.index4, name="accessories-index4",),
